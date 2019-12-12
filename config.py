6703a967f4dfe85945140f937fce9a69506b0b3a
Joke Funny <dlwxxxdlw@gmail.com>
2019-12-12 14:27:58 +0800

    upload config.py for wsl2
    upload config.py for wsl2

--- config.py
+++ config.py
@@ -6,11 +6,13 @@ import socket
 
 from libqtile import bar, hook, layout
 from libqtile.command import lazy
-from libqtile.config import Drag, Group, Key, Screen, ScratchPad, DropDown
-from libqtile.widget import (Battery, Clock, CurrentLayout, CurrentLayoutIcon,
+from libqtile.config import Drag, Group, Key, Screen, ScratchPad, DropDown, Match
+from libqtile.widget import (Battery, BatteryIcon, Clock, CurrentLayout, CurrentLayoutIcon,
                              GroupBox, Notify, Prompt, Sep, Systray, TaskList,
-                             TextBox, LaunchBar, Wallpaper)
-from libqtile.extension.dmenu import Dmenu
+                             TextBox, LaunchBar, Wallpaper, Cmus, Pacman)
+from libqtile.extension.dmenu import DmenuRun
+from libqtile.extension.window_list import WindowList
+from libqtile.extension import CommandSet
 
 DEBUG = os.environ.get("DEBUG")
 HOME = os.path.expanduser("~") + "/"
@@ -81,11 +83,16 @@ def switch_screens():
 def set_floating(window):
     floating_types = ["notification", "toolbar", "splash", "dialog"]
     floating_roles = ["EventDialog", "Msgcompose", "Preferences"]
-    floating_names = ["Terminator Preferences"]
+    floating_names = ["Terminator Preferences", "首选项", "设置", "qq"]
+
+    def judgeby_names(window_name):
+        for name in floating_names:
+            if name in window_name:
+                return True
 
     if (window.window.get_wm_type() in floating_types
             or window.window.get_wm_window_role() in floating_roles
-            or window.window.get_name() in floating_names
+            or judgeby_names(window.window.get_name())
             or window.window.get_wm_transient_for()):
         window.floating = True
 
@@ -94,28 +101,82 @@ def init_keys():
     keys = [
         Key([mod], "p", lazy.screen.prev_group(skip_managed=True)),
         Key([mod], "n", lazy.screen.next_group(skip_managed=True)),
-        Key([mod, "shift"], "Left", window_to_prev_group()),
-        Key([mod, "shift"], "Right", window_to_next_group()),
-        Key([mod, "mod1"], "Left", lazy.prev_screen()),
-        Key([mod, "mod1"], "Right", lazy.next_screen()),
-        Key([mod, "shift", "mod1"], "Left", window_to_prev_screen()),
-        Key([mod, "shift", "mod1"], "Right", window_to_next_screen()),
-        Key([mod], "t", switch_screens()),
-        Key([mod], "l", lazy.group.next_window()),
-        Key([mod], "h", lazy.group.prev_window()),
+        # Key([mod, "shift"], "Left", window_to_prev_group()),
+        # Key([mod, "shift"], "Right", window_to_next_group()),
+        # Key([mod, "mod1"], "Left", lazy.prev_screen()),
+        # Key([mod, "mod1"], "Right", lazy.next_screen()),
+        # Key([mod, "shift", "mod1"], "Left", window_to_prev_screen()),
+        # Key([mod, "shift", "mod1"], "Right", window_to_next_screen()),
+        Key([mod, "shift"], "t", switch_screens()),
+        # Key([mod, "shift"], "l", lazy.group.next_window()),
+        # Key([mod, "shift"], "h", lazy.group.prev_window()),
         Key([mod], "space", lazy.next_layout()),
+        Key([mod], "h", lazy.layout.left()),
         Key([mod], "j", lazy.layout.up()),
         Key([mod], "k", lazy.layout.down()),
+        Key([mod], "l", lazy.layout.right()),
+        Key([mod], "bracketleft", lazy.layout.swap_left()),
+        Key([mod], "bracketright", lazy.layout.swap_right()),
+        Key([mod, "shift"], "j", lazy.layout.shuffle_down()),
+        Key([mod, "shift"], "k", lazy.layout.shuffle_up()),
+        Key([mod, "shift"], "h", lazy.layout.shuffle_left()),
+        Key([mod, "shift"], "l", lazy.layout.shuffle_right()),
+        Key([mod, "control"], "j", lazy.layout.grow_down()),
+        Key([mod, "control"], "k", lazy.layout.grow_up()),
+        Key([mod, "control"], "h", lazy.layout.grow_left()),
+        Key([mod, "control"], "l", lazy.layout.grow_right()),
+        Key([mod, "shift"], "n", lazy.layout.normalize()),
+        Key([mod], "o", lazy.layout.maximize()),
+        Key([mod, "shift"], "space", lazy.layout.flip()),
+        Key([mod], "Return", lazy.layout.toggle_split()),
+        # Key([mod], "Return", lazy.spawn(terminal)),
         Key([mod], "f", lazy.window.toggle_floating()),
         Key([mod], "r", lazy.spawncmd()),
         # yaourt i3lock
-        Key([mod, "mod1"], "l", lazy.spawn("i3lock")),
+        Key([mod],"BackSpace", lazy.spawn("i3lock -i /home/dlwxxxdlw/Pictures/wallpapers/2.png")),
         Key([mod], "u", lazy.spawn(browser_chromium)),
-        Key([mod], "x", lazy.spawn(browser_firefox)),
-        Key([mod], "Return", lazy.spawn(terminal)),
-        Key([mod], "BackSpace", lazy.window.kill()),
+        # Key([mod], "x", lazy.spawn(browser_firefox)),
+        Key([mod], "p", lazy.spawn(package_manager)),
+        Key([mod], "n", lazy.spawn(file_manager)),
+        Key([mod], "x", lazy.window.kill()),
         Key([mod, "control"], "r", lazy.restart()),
         Key([mod, "control"], "q", lazy.shutdown()),
+        Key([mod, "shift"], 'r', lazy.run_extension(DmenuRun(
+            dmenu_prompt=">",
+            dmenu_font="DejaVu Sans Mono,14",
+            background="#15181a",
+            foreground="#00ff00",
+            selected_background="#079822",
+            selected_foreground="#fff",
+            # dmenu_height=24,  # Only supported by some dmenu forks
+        ))),
+        Key([mod, "shift"], 'm', lazy.run_extension(CommandSet(
+            commands={
+                'play': 'cmus-remote -p',
+                'pause': 'cmus-remote -u',
+                'stop': 'cmus-remote -s',
+                'prev': 'cmus-remote -r',
+                'next': 'cmus-remote -n',
+                'repeat': 'cmus-remote -R',
+                'shuffle': 'cmus-remote -S',
+                '5up': 'cmus-remote -v +5%',
+                '5down': 'cmus-remote -v -5%',
+                '10down': 'cmus-remote -v -10%',
+                '10up': 'cmus-remote -v +10%',
+                '15up': 'cmus-remote -v +15%',
+                '15down': 'cmus-remote -v -15%',
+                '20down': 'cmus-remote -v -20%',
+                '20up': 'cmus-remote -v +20%',
+            }
+        ))),
+        Key([mod, "shift"], 'w', lazy.run_extension(WindowList(
+            dmenu_prompt=">",
+            dmenu_font="DejaVu Sans Mono,14",
+            background="#15181a",
+            foreground="#00ff00",
+            selected_background="#079822",
+            selected_foreground="#fff",
+        ))),
         # region this "scrot" app can use -s to select an area of screen
         # Key([], "Print", lazy.spawn("scrot")),
         # Key([mod], "s", lazy.spawn("scrot -s '%Y-%m-%d_$wx$h.png' -e 'mv $f /home/dlwxxxdlw/Screenshots/'")),
@@ -123,14 +184,14 @@ def init_keys():
         # Key([mod], "s", lazy.spawn("scrot -e 'mv $f /home/dlwxxxdlw/Screenshots/'")),
         # Key([], "Scroll_Lock", lazy.spawn(HOME + ".local/bin/i3lock -d")),
         # endregion
-        Key([mod], "Delete", lazy.spawn("amixer set Master toggle")),
-        Key([mod], "Prior", lazy.spawn("amixer set Master 5+")),
-        Key([mod], "Next", lazy.spawn("amixer set Master 5-")),
-        Key([mod], "Insert",
-            lazy.spawn(HOME + ".local/bin/spotify-dbus playpause")),
-        Key([mod], "End", lazy.spawn(HOME + ".local/bin/spotify-dbus next")),
-        Key([mod], "Home",
-            lazy.spawn(HOME + ".local/bin/spotify-dbus previous")),
+        # Key([mod], "Delete", lazy.spawn("amixer set Master toggle")),
+        # Key([mod], "Prior", lazy.spawn("amixer set Master 5+")),
+        # Key([mod], "Next", lazy.spawn("amixer set Master 5-")),
+        # Key([mod], "Insert",
+        #     lazy.spawn(HOME + ".local/bin/spotify-dbus playpause")),
+        # Key([mod], "End", lazy.spawn(HOME + ".local/bin/spotify-dbus next")),
+        # Key([mod], "Home",
+        #     lazy.spawn(HOME + ".local/bin/spotify-dbus previous")),
     ]
     if DEBUG:
         keys += [
@@ -159,6 +220,12 @@ def init_groups():
     def _inner(key, name):
         keys.append(Key([mod], key, lazy.group[name].toscreen()))
         keys.append(Key([mod, "shift"], key, lazy.window.togroup(name)))
+        if key == "equal":
+            return Group(name, spawn="ss-qt5")
+        elif name == "01":
+            return Group(name, spawn="terminator")
+        elif name == "02":
+            return Group(name, spawn="chromium")
         return Group(name)
 
     # groups = [("dead_grave", "00")]
@@ -167,10 +234,10 @@ def init_groups():
     res_groups = [_inner(*i) for i in groups]
     res_groups += [
         ScratchPad("scratchpad",
-                   [DropDown("fish", "roxterm", height=0.5, opacity=0.6)])
+                   [DropDown("scratch", "kitty", height=0.65, opacity=0.45)])
     ]
     keys.append(
-        Key([], 'Pause', lazy.group['scratchpad'].dropdown_toggle('fish')))
+        Key([mod], 's', lazy.group['scratchpad'].dropdown_toggle('scratch')))
     return res_groups
 
 
@@ -183,19 +250,21 @@ def init_widgets():
     widgets = [
         Prompt(
             prompt=prompt,
-            font="DejaVu Sans Mono",
+            font="DejaVu Sans Mono for Powerline Bold",
             padding=10,
             background=GREY),
         TextBox(
-            text="◤ ",
-            fontsize=45,
-            padding=-8,
-            foreground=GREY,
+            text="◤",
+            fontsize=25,
+            padding=-1,
+            foreground=ORANGE,
             background=DARK_GREY),
+        CurrentLayout(),
         CurrentLayoutIcon(scale=0.6, padding=-4),
         TextBox(text=" ", padding=2),
         GroupBox(
-            fontsize=8,
+            fontsize=12,
+            font="DejaVu Sans Mono for Powerline Bold",
             padding=4,
             borderwidth=1,
             urgent_border=DARK_BLUE,
@@ -207,47 +276,78 @@ def init_widgets():
             other_current_screen_border=ORANGE),
         TextBox(
             text="◤",
-            fontsize=45,
+            fontsize=25,
             padding=-1,
-            foreground=DARK_GREY,
+            foreground=ORANGE,
             background=GREY),
         TaskList(
+            fontsize=13,
+            font="DejaVu Sans Mono for Powerline Bold",
             borderwidth=0,
             highlight_method="block",
             background=GREY,
-            border=DARK_GREY,
+            border=BLUE,
             urgent_border=DARK_BLUE),
-        Systray(background=GREY),
+        Cmus(
+            fontsize=13,
+            font="DejaVu Sans Mono for Powerline Bold",
+        ),
+        Pacman(
+            fontsize=13,
+            font="DejaVu Sans Mono for Powerline Bold",
+        ),
+        Systray(background=DARK_ORANGE),
         Wallpaper(
             directory="/home/dlwxxxdlw/Pictures/wallpapers",
+            fontsize=13,
+            font="DejaVu Sans Mono for Powerline Bold",
+            label="(´･_･`)",
             random_selection=True,
             wallpaper_command=['feh', '--bg-max']
         ),
         # LaunchBar needs some dependencies, use yaourt to install them
-        LaunchBar(progs=[
-            (  # yaourt thunderbird virtualbox
-                'thunderbird', 'thunderbird', 'launch thunderbird'),
-            ('virtualbox', 'virtualbox', 'launch virtualbox'),
-            ('thunar', 'thunar', 'launch thunar'),
-            (
-                'aria',  # get this from github
-                'firefox --new-tab ~/Downloads/aria-ng/index.html',
-                'aria'),
-            ('shutter', 'shutter', 'launch shutter'),
-            ('ss', 'ss-qt5', 'launch shadowsocks-qt5'),
-            ('Tim', '/opt/deepinwine/apps/Deepin-TIM/run.sh', 'launch Tim'),
-            ('Thunder', '/opt/deepinwine/apps/Deepin-ThunderSpeed/run.sh', 'launch Thunder')
-        ]),
+        # LaunchBar(
+        #     progs=[
+            # (  # yaourt thunderbird virtualbox
+            #     'thunderbird', 'thunderbird', 'launch thunderbird'),
+            # ('virtualbox', 'virtualbox', 'launch virtualbox'),
+                # ('thunar', 'thunar', 'launch thunar'),
+                # ('terminator', 'terminator', 'launch terminator'),
+            # (
+            #     'aria',  # get this from github
+            #     'firefox --new-tab ~/Downloads/aria-ng/index.html',
+            #     'aria'),
+            # ('shutter', 'shutter', 'launch shutter'),
+                # ('ss', 'ss-qt5', 'launch shadowsocks-qt5'),
+            # ('Tim', '/opt/deepinwine/apps/Deepin-TIM/run.sh', 'launch Tim'),
+            # ('Thunder', '/opt/deepinwine/apps/Deepin-ThunderSpeed/run.sh', 'launch Thunder')
+            # ],
+            # fontsize=13,
+            # font="DejaVu Sans Mono for Powerline Bold",
+                  # ),
         TextBox(
             text="◤",
-            fontsize=45,
+            fontsize=25,
             padding=-1,
-            foreground=GREY,
+            foreground=ORANGE,
             background=DARK_GREY),
-        TextBox(text=" ⚠", foreground=BLUE, fontsize=18),
-        Notify(),
-        TextBox(text=" ⌚", foreground=BLUE, fontsize=18),
-        Clock(format="%A %d-%m-%Y %H:%M")
+        # TextBox(text=" ⚠", foreground=BLUE, fontsize=18),
+        Notify(
+            fontsize=16,
+            font="DejaVu Sans Mono for Powerline Bold",
+            background=BLUE
+        ),
+        # TextBox(
+        #     text="⌚", 
+        #     foreground=BLUE, 
+        #     fontsize=16,
+        #     font="DejaVu Sans Mono for Powerline Bold",
+        # ),
+        Clock(
+            fontsize=13,
+            font="DejaVu Sans Mono for Powerline Bold",
+            format="%A %d-%m-%Y %H:%M"
+        )
     ]
     if hostname in ("spud", "saiga"):
         widgets[-2:-2] = [
@@ -280,7 +380,8 @@ def init_layouts(num_screens):
         layout.Tile(
             ratio=0.5,
             margin=margin,
-            border_width=1,
+            border_focuts="#ff0000",
+            border_width=4,
             border_normal="#111111",
             border_focus=BLUE)
     ])
@@ -318,20 +419,27 @@ def startup():
     start the applications when qtile startup
     :return: None
     """
-    execute_once("nm-applet")  # yaourt network manager applet
-    execute_once("fcitx")  # yaourt fcitx
-    execute_once("aria2c --conf-path=/home/dlwxxxdlw/.config/aria2/aria2.conf")
-    execute_once("source /home/dlwxxxdlw/.bashrc")
+    # execute_once("nm-applet")  # yaourt network manager applet
+    execute_once("fcitx &")  # yaourt fcitx
+    execute_once("nohup albert > /dev/null 2>albert.log &")
+    # execute_once("ss-qt5")
+    # execute_once("aria2c --conf-path=/home/dlwxxxdlw/.config/aria2/aria2.conf")
+    # execute_once("source /home/dlwxxxdlw/.bashrc")
+    pass
 
 
 if __name__ in ["config", "__main__"]:
     if HOME + ".local/bin" not in os.environ["PATH"]:
         os.environ["PATH"] = HOME + ".local/bin:{}".format(os.environ["PATH"])
 
-    mod = "mod4"
+    # mod = "mod4"
+    mod = "mod1"
     browser_chromium = "chromium"  # yaourt chromium
     browser_firefox = "firefox"  # yaourt firefox
-    terminal = "roxterm"  # yaourt roxterm
+    # terminal = "roxterm"  # yaourt roxterm
+    terminal = "terminator"  # yaourt roxterm
+    package_manager = "octopi"
+    file_manager = "nautilus"
     hostname = socket.gethostname()
     # follow_mouse_focus = True # not sure what this means
     # never set "cursor_warp" True ,it will make your mouse
@@ -344,11 +452,25 @@ if __name__ in ["config", "__main__"]:
     # focus_on_window_activation = "smart"
     groups = init_groups()
     floating_layout = init_floating_layout()
-    layouts = [layout.Max()]
+    layouts = [
+        layout.Max(),
+        layout.Bsp(
+            border_focuts="#ff0000",
+            border_width=4,
+        ),
+        layout.MonadTall(
+            border_focuts="#ff0000",
+            border_width=4,
+        ),
+        layout.Matrix(
+            border_focuts="#ff0000",
+            border_width=4,
+        ),
+    ]
     screens = [Screen(top=init_top_bar())]
     widget_defaults = init_widgets_defaults()
     # region don't know how to use Dmenu
-    Dmenu(command=["shutdown", "reboot"], dmenu_font="DejaVu Sans Mono"),
+    # Dmenu(command=["shutdown", "reboot"], dmenu_font="DejaVu Sans Mono"),
     # endregion
 
     if DEBUG:
